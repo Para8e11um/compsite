d2bf247b75934567d39f1f5ff4a11daeb3be0f3d
Para8e11um <para8e11um1@gmail.com>
2023-04-29 14:20:37 +0600

    fixing display scaling on mobile 1

--- windowresize.js
+++ windowresize.js
@@ -1,8 +1,13 @@
 let windowWidth;
 
 function changeMainWidth() {
+    if (window.innerWidth > window.innerHeight && window.innerHeight < 1200) {
 windowWidth = 700000 / window.innerWidth + 1700;
 document.documentElement.style.setProperty(`--mainheight`, `${windowWidth}px`);
+} else {
+    windowWidth = 1400000 / window.innerWidth + 1700;
+    document.documentElement.style.setProperty(`--mainheight`, `${windowWidth}px`);
+}
 };
 
 window.onresize = changeMainWidth;
