4be19c916946bd0e935a010dc0271f35a71c73d2
Para8e11um <para8e11um1@gmail.com>
2023-04-29 21:41:11 +0600

    a miracle

--- windowresize.js
+++ windowresize.js
@@ -1,13 +1,26 @@
 let windowWidth;
+let mainTop;
+
+let mainClass = document.querySelector("#mainid");
+let style = window.getComputedStyle(mainClass);
+windowWidth = style.getPropertyValue('height');
+console.log(windowWidth);
+document.documentElement.style.setProperty(`--mainheight`, `${windowWidth}`);
+mainTop = style.getPropertyValue('top');
+console.log(mainTop);
+document.documentElement.style.setProperty(`--maintop`, `${mainTop}`);
 
 function changeMainWidth() {
-    if (window.innerWidth > window.innerHeight && window.innerHeight < 1200) {
-windowWidth = 700000 / window.innerWidth + 1700;
-document.documentElement.style.setProperty(`--mainheight`, `${windowWidth}px`);
-} else {
-    windowWidth = 1400000 / window.innerWidth + 1700;
-    document.documentElement.style.setProperty(`--mainheight`, `${windowWidth}px`);
-}
+    mainClass = document.querySelector("#mainid");
+    style = window.getComputedStyle(mainClass);
+    windowWidth = style.getPropertyValue('height');
+    console.log(windowWidth);
+    document.documentElement.style.setProperty(`--mainheight`, `${windowWidth}`);
+    mainTop = style.getPropertyValue('top');
+    console.log(mainTop);
+    document.documentElement.style.setProperty(`--maintop`, `${mainTop}`);
 };
+
+changeMainWidth();
 
 window.onresize = changeMainWidth;
