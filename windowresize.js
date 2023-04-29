5f8ee1d7b2cc5efe85dd59bdafbcf1022fd86059
Para8e11um <para8e11um1@gmail.com>
2023-04-29 23:25:55 +0600

    text formatting and arrow deco

--- windowresize.js
+++ windowresize.js
@@ -4,20 +4,16 @@ let mainTop;
 let mainClass = document.querySelector("#mainid");
 let style = window.getComputedStyle(mainClass);
 windowWidth = style.getPropertyValue('height');
-console.log(windowWidth);
 document.documentElement.style.setProperty(`--mainheight`, `${windowWidth}`);
 mainTop = style.getPropertyValue('top');
-console.log(mainTop);
 document.documentElement.style.setProperty(`--maintop`, `${mainTop}`);
 
 function changeMainWidth() {
     mainClass = document.querySelector("#mainid");
     style = window.getComputedStyle(mainClass);
     windowWidth = style.getPropertyValue('height');
-    console.log(windowWidth);
     document.documentElement.style.setProperty(`--mainheight`, `${windowWidth}`);
     mainTop = style.getPropertyValue('top');
-    console.log(mainTop);
     document.documentElement.style.setProperty(`--maintop`, `${mainTop}`);
 };
 
